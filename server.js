58d186be5853171f0937370623e99603f1a33f84
Dylan Palmboom <nofxboy1234@gmail.com>
2022-06-16 10:49:10 +0200

    Connect node app to database

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+const { MongoClient } = require('mongodb');
 const express = require('express');
+let db;
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  const allAnimals = await db.collection('animals').find().toArray();
+  console.log(allAnimals);
   res.send('Welcome to the homepage');
 });
 
@@ -10,4 +14,15 @@ app.get('/admin', (req, res) => {
   res.send('This is the top secret admin page');
 });
 
-app.listen(3000);
+async function start() {
+  // mongodb://localhost:27017
+  // "mongodb://root:root@localhost:27017/AmazingMernApp?&authSource=admin"
+  // "mongodb://localhost:27017/AmazingMernApp?&authSource=admin"
+  const client = new MongoClient(
+    'mongodb://localhost:27017/AmazingMernApp?&authSource=admin'
+  );
+  await client.connect();
+  db = client.db();
+  app.listen(3000);
+}
+start();
