431d54bb81fab24765351b947173ada8e17b11c6
Dylan Palmboom <nofxboy1234@gmail.com>
2022-06-17 12:52:07 +0200

    Password protect admin page

--- server.js
+++ server.js
@@ -7,10 +7,22 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
+function passwordProtected(req, res, next) {
+  res.set('WWW-Authenticate', "Basic realm='Our MERN App'");
+  if (req.headers.authorization == 'Basic YWRtaW46YWRtaW4=') {
+    next();
+  } else {
+    console.log(req.headers.authorization);
+    res.status(401).send('Try again');
+  }
+}
+
 app.get('/', async (req, res) => {
   const allAnimals = await db.collection('animals').find().toArray();
   res.render('home', { allAnimals });
 });
+
+app.use(passwordProtected);
 
 app.get('/admin', (req, res) => {
   res.render('admin');
